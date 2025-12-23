98aca1af72a7569860a4d07cd5e37675050859fa
Riya Choudhary <riyachoudhary04343@gmail.com>
2025-12-24 00:18:00 +0530

    Added input and basic operations practice.

---
+++ day2.1_basics.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello", name)
+print("You are", age, "years old")
+
+# Basic operators
+a = 10
+b = 5
+print("Sum:", a+b)
+print("Difference:", a-b)
+print("Product:", a*b)
+print("Division:", a/b)
