077d5d1321bb7301ce347835d02b5d961770ace8
Riya Choudhary <riyachoudhary04343@gmail.com>
2025-12-24 00:24:47 +0530

    simple calculator with inputs.

---
+++ day2.2_basics.py
@@ -0,0 +1,17 @@
+a = int(input("enter first no.: "))
+b = int(input("enter second no.: "))
+
+
+print("the sum of", a, "and", b, "is", a+b)
+
+print("the sub of", a, "and", b, "is", a-b)
+
+print("the div of", a, "and", b, "is", a/b)
+
+print("the flr of", a, "and", b, "is", a//b)
+
+print("the exp of", a, "and", b, "is", a**b)
+
+print("the mul of", a, "and", b, "is", a*b)
+
+print("the mod of", a, "and", b, "is", a%b)
