0ea2160aa6730f079549968ce56e3d17dd4453b1
Riya Choudhary <riyachoudhary04343@gmail.com>
2025-12-25 22:33:38 +0530

    added nested if elif else and match statement example program

---
+++ day4_basics.py
@@ -0,0 +1,29 @@
+marks = int(input("enter yoiur marks: "))
+
+if marks >= 0:
+    if marks >= 90:
+        print("grade = A+")
+    elif marks >=80:
+        print("grade = A")
+    elif marks >=60:
+        print("grade = B")
+    elif marks >= 40:
+        print("grade = C")
+    else:
+        print("FAIL")
+
+
+#match statement:-
+
+choice = int(input("enter a num(1-4): "))
+match choice:
+    case 1:
+        print("selected c++ ")
+    case 2:
+        print("selected java ")
+    case 3:
+        print("selected python ")
+    case 4: 
+        print("selected html ")
+    case _: #used for default
+        print("invalid choice ")
