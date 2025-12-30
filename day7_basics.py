1a363baf79110355dffa9f7f9083998b25568ea9
Riya Choudhary <riyachoudhary04343@gmail.com>
2025-12-30 23:25:31 +0530

    added simple program demonstrating break and continue theory

---
+++ day7_basics.py
@@ -0,0 +1,11 @@
+print("hello riya:")
+for i in range(0, 5):
+    if i == 4:
+        continue
+    print(i)
+
+print("hello ma'am:")
+for j in range(0, 5):
+    if j == 5:
+        break
+    print(j)
