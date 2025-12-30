84af5ead0874a91d5a0fc35f8e2ecd5221ceb719
Riya Choudhary <riyachoudhary04343@gmail.com>
2025-12-30 23:12:37 +0530

    added program for functions and argument

---
+++ day6_basics.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    print("sum: ", a+b)
+
+
+def greet(name = "students"):
+    print("hello,", name)
+
+
+def student_info(name,age,course):
+    print("name: ", name)
+    print("age: ", age)
+    print("course: ", course)
+
+
+add(4,5)
+
+greet()
+greet("riya")
+
+student_info("riya","19","btech")
