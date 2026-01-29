112f06a42d44ea3619591daf8a53bce4069189ca
Riya Choudhary <riyachoudhary04343@gmail.com>
2026-01-30 04:55:14 +0530

    Completed dictionary methods practice

---
+++ day12_basics.py
@@ -0,0 +1,14 @@
+students = {
+    "name": "riya choudhary",
+    "age": 19,
+    "branch": "cse"
+}
+
+students["age"]= 20
+students["college"] = "abc college"
+print(students.get("branch"))
+
+print(students.keys())
+print(students.values())
+print(students.items())
+print (students)
