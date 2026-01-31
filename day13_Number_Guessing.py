e352c71c34e0a0e38772a8e309513c1705adee6e
Riya Choudhary <riyachoudhary04343@gmail.com>
2026-01-31 05:36:41 +0530

    Add comments for clarity in number guessing game
    Added comments to clarify the purpose of the code.

--- day13_Number_Guessing.py
+++ day13_Number_Guessing.py
@@ -1,8 +1,12 @@
+# importing random module to generate random numbers
 import random
 number = random.randint(1,10)
 guess = None
+# Loop will run until user guesses the correct number
 while guess!= number:
     guess = int(input("guess the number(1-10): "))
+    
+    # Checking if guess is lower, higher, or correct
     if guess<number:
         print("too low")
     elif guess>number:
