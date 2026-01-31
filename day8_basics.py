698a2daf1ce7cc42d9a6743f300ed0f14b3601a9
Riya Choudhary <riyachoudhary04343@gmail.com>
2026-01-31 06:12:31 +0530

    Refactor comments in day8_basics.py
    Updated comments for clarity and consistency in list operations.

--- day8_basics.py
+++ day8_basics.py
@@ -1,16 +1,17 @@
+# List Examples & Methods
 l = [12,7,45,1,2,4,6,1,1]
 print(l)
-l.append(7)
-l.sort()
-l.sort(reverse=True)
-l.reverse()
-print(l.index(1))
+l.append(7)  # add element at end
+l.sort() # sort ascending
+l.sort(reverse=True) # sort descending
+l.reverse() # reverse list
+print(l.index(1)) # first index of 1
 print(l.count(1))
 m = l.copy()
 m[1]=100
-l.insert(1, 899)
+l.insert(1, 899) # insert element at index 1
 m= [900, 1000, 1100]
-l.extend(m)
+l.extend(m)   # add multiple elements
 k = l+m
 print(k)
 print(l)
