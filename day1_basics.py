6b0c11104d9caba85c6707e39151072f8ef83bb9
Riya Choudhary <riyachoudhary04343@gmail.com>
2026-01-31 05:57:39 +0530

    added comments

--- day1_basics.py
+++ day1_basics.py
@@ -1,8 +1,11 @@
 print("Hello World")
 print("My name is Riya")
 
+
+# Storing values in variables
 name = "Riya"
 age = 20
 
+# Printing variables
 print("My name is", name)
 print("My age is", age)
