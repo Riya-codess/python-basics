328a751d509f2dceb7986673aad4f6c6066a7f5f
Riya Choudhary <riyachoudhary04343@gmail.com>
2026-01-31 06:13:47 +0530

    Update day9_basics.py
    Added an example of tuple concatenation to demonstrate how to combine tuples in Python.

--- day9_basics.py
+++ day9_basics.py
@@ -1,9 +1,10 @@
+# Tuple Examples & Methods
 tup = (0,1,2,3,2,3,1,3,2)
 print("numbers: ", tup)
-res = tup.count(3)
+res = tup.count(3) # count occurrence
 print(res)
 print("first num. : ", tup[0])
 print("index: ", tup.index(3))
 print("length: ", len(tup))
-new_tup= tup+(40,50)
+new_tup= tup+(40,50) # concatenate tuples
 print("concatenation: ", new_tup)
