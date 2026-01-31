f38b3c841717542eda7896ef3827f5b0357d71d5
Riya Choudhary <riyachoudhary04343@gmail.com>
2026-01-31 05:40:08 +0530

    Update student age and add college to dictionary
    Updated the age of the student and added college information. Accessed and printed various dictionary methods.

--- day12_basics.py
+++ day12_basics.py
@@ -1,13 +1,16 @@
+# Creating a dictionary to store student details
 students = {
     "name": "riya choudhary",
     "age": 19,
     "branch": "cse"
 }
 
+# Updating existing value
 students["age"]= 20
 students["college"] = "abc college"
-print(students.get("branch"))
 
+# Accessing values using dictionary methods
+print(students.get("branch"))
 print(students.keys())
 print(students.values())
 print(students.items())
