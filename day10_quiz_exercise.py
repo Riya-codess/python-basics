afc4680f42ed039c8e858dcdfb56c802a0fa9188
Riya Choudhary <riyachoudhary04343@gmail.com>
2026-01-31 05:42:28 +0530

    added comments
    Added a quiz game with questions and prize money.

--- day10_quiz_exercise.py
+++ day10_quiz_exercise.py
@@ -1,7 +1,10 @@
+# List of questions and their correct answers
 questions = [["what is the capital of India?", "Delhi"],
              ["which language is used for web development?", "Python"],
              ["who is known as the father of computer?", "charles babbage"],
              ]
+
+# Prize money for each question
 prize_money = [1000, 2000, 3000, 5000]
 total_amount = 0
 
@@ -10,6 +13,8 @@ print("welcome to KBC quiz game\n")
 for i in range(len(questions)):
     print("Question", i+1, ":", questions[i][0])
     answer = input("your answer: ")
+
+      # Checking if user's answer matches the correct answer
     if answer.lower() == questions[i][1].lower():
         total_amount = prize_money[i]
         print("correct answer ✅")
@@ -17,5 +22,8 @@ for i in range(len(questions)):
     else:
         print("wrong answer")
         break
+
+
+# Displaying final result
 print("game over")
 print("total amount you are taking home is : ", total_amount)
